6d06a585f2bdea3b052dc843ed426a21cf935de1
Udhaya0519 <udhaya0519@gmail.com>
2024-08-26 10:07:30 +0530

    updated js

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
 document.addEventListener("DOMContentLoaded",()=>{
 var menubar = document.querySelector(".head-wrapper > div:nth-child(4) >i")
 var  section = document.querySelectorAll(".head-wrapper > div:nth-child(2) >ul >li >a")
-    
+
 var toLeft = document.querySelector(".head-wrapper>:nth-child(2)>ul")
 const mediaQuery = window.matchMedia("(max-width: 800px)")
+let toggle = false;
 
 function mediaTOset(e){
 
     if (e.matches) {
+
         menubar.addEventListener("click",()=>{
-               toLeft.style.left="-4%"
+            toggle=!toggle
+            console.log(toggle);
+            
+            if (toggle===true) {
+                toLeft.style.left="-4%"
                toLeft.style.transition ="1s"
+                
+            } else if(toggle===false) {
+                toLeft.style.left="-70%"
+               toLeft.style.transition ="1s"
+                
+            }
+           
         })
       
         section.forEach((el)=>{
